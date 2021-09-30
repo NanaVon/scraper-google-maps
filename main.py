3cc5c9471ba5f996db45a94aca526b0f27780690
Jorge D'Ecclesiis Caceres <soyhattricks@gmail.com>
2021-09-30 16:29:33 -0300

    change the input logic of arg

--- main.py
+++ main.py
@@ -2,23 +2,21 @@
 
 import urllib.request
 from time import sleep
+import sys
 
-
- 
-def main(cantRow, cantColumn, CordStartY, CordStartX,timeColumn=30, timeRow:1800):
+def main(cantRow, CordStartY, CordStartX,CordEndY,CordEndX,timeColumn=30, timeRow:1800):
     fileName='{}y{}x.jpg'
-    for posy in range(cantRow):
-        for posx in range(cantColumn):       
+    for posy in range(CordStartY,CordEndY):
+        for posx in range(CordStartX, CordEndX):       
             print('posy:', posy, 'posx:', posx)
-            urllib.request.urlretrieve("https://khms3.google.com/kh/v=908?x={}&y={}&z=21".format(CordStartX,CordStartY), fileName.format(posy,posx))
-            cordX=cordX+1
+            urllib.request.urlretrieve("https://khms3.google.com/kh/v=908?x={}&y={}&z=21".format(posx,posy), fileName.format(posy,posx))
             sleep(TimeColumn)
         sleep(timeRow)
-        cordY=cordY+1
-        cordX=timeRow
         print("posy:",posy)
 
 if __name__== '__main__':
+    args=sys.argv()
+        
     main()
 
 
