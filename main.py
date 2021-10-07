8c68558c95fb3d1afec5c394679f5652b01e9f71
NanaVon <vonl07j@gmail.com>
2021-10-07 09:59:53 -0300

    correct argument names

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import urllib.request
 from time import sleep
 import sys
 
-def main(cantRow, CordStartY, CordStartX,CordEndY,CordEndX,timeColumn=30, timeRow:1800):
+def main(CoordStartX, CoordStartY,CoordEndX,CoordEndY,timeBetweenReq=30, timeBetweenBatches=1800, zoom:int):
     fileName='{}y{}x.jpg'
     for posy in range(CordStartY,CordEndY):
         for posx in range(CordStartX, CordEndX):       
             print('posy:', posy, 'posx:', posx)
-            urllib.request.urlretrieve("https://khms3.google.com/kh/v=908?x={}&y={}&z=21".format(posx,posy), fileName.format(posy,posx))
-            sleep(TimeColumn)
-        sleep(timeRow)
+            urllib.request.urlretrieve("https://khms3.google.com/kh/v=908?x={}&y={}&z={}".format(posx,posy, zoom), fileName.format(posy,posx))
+            sleep(timeBetweenReq)
+        sleep(timeBetweenBatches)
         print("posy:",posy)
 
 if __name__== '__main__':
